45338dcb71ca105290a30357c86d327266fea058
Paul MEKHAIL <paul.mekhail4@pm.me>
2025-03-22 16:30:31 +0100

    petite modif

--- init.lua
+++ init.lua
@@ -1,3 +1,2 @@
-require("paul.lazy")
 require("paul.set")
 require("paul.remap")
--- lua/paul/set.lua
+++ lua/paul/set.lua
@@ -1,9 +1,10 @@
-vim.opt.nu.nru = true
+-- Activer la numérotation des lignes
+vim.o.number = true
 
 -- Créer un groupe d'autocommandes
 local numbertoggle_group = vim.api.nvim_create_augroup("numbertoggle", { clear = true })
 
--- Définir les autocommandes
+-- Définir les autocommandes pour activer les numéros de ligne relatifs
 vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
   group = numbertoggle_group,
   pattern = "*",
@@ -14,6 +15,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnte
   end,
 })
 
+-- Définir les autocommandes pour désactiver les numéros de ligne relatifs
 vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
   group = numbertoggle_group,
   pattern = "*",
@@ -23,6 +25,7 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave"
     end
   end,
 })
+
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
