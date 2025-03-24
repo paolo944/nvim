a07ff39ab746fd6065a26a4c2d91ba4dedf16a60
Paul Mekhail <paul@mekhail.dev>
2025-03-25 00:54:37 +0100

    added neogit

--- lua/paul/remap.lua
+++ lua/paul/remap.lua
@@ -26,3 +26,6 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-m>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
